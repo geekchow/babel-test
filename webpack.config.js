314438946de262b72a80f981fd0c8cd00d890dd3
Phil G S Zhou <phil.g.s.zhou@noexternalmail.hsbc.com>
2016-12-26 21:00:05 +0800

    init version of babel test

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports= {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+
+    module: {
+        loaders:[
+            {
+                test: /\.js$/,
+                include: [path.resolve(__dirname, 'src')],
+                loader: 'babel?presets[]=es2015',
+            }
+        ]
+    }
+}
